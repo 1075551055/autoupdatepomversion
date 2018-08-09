8b7e82868b2b3c0ca21c5d3feeebbbc5d651e9cc
Water Li <water.li@oocl.com>
2018-08-09 16:02:47 +0800

    fix bug:cannot push to central server when origin branch is different from central branch

--- src/main/java/core/PushCommitToRemote.java
+++ src/main/java/core/PushCommitToRemote.java
@@ -1,13 +1,12 @@
 package core;
 
-import org.eclipse.jgit.api.CreateBranchCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.PushCommand;
 import org.eclipse.jgit.api.RemoteAddCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.transport.RefSpec;
 import org.eclipse.jgit.transport.URIish;
 
 import java.io.File;
@@ -16,24 +15,37 @@ import java.net.URISyntaxException;
 
 public class PushCommitToRemote {
 
-    public static void pushChangesToRemote(Repository existingRepository, String originGitUrl, String hkgGitUrl, String remoteRepository) throws GitAPIException, URISyntaxException, IOException {
-        pushChangesToOrigin(existingRepository, originGitUrl);
-        pushChangesToUpstream(existingRepository, hkgGitUrl, remoteRepository);
+    public static void main(String[] args) throws IOException, GitAPIException, URISyntaxException {
+        Repository existingRepository = new FileRepositoryBuilder()
+                .setGitDir(new File("C:/gitlab/iris4_dsh_demo/.git"))
+                .build();
+        String originGitUrl = "git@zha-ir4-ci1-w10:liwa/demo.git";
+        String hkgGitUrl = "git@zha-ir4-ci1-w10:dsh/demo.git";
+        String centralBranchForPush = "upstream/AutoUpdatePomRemote";
+        pushChangesToRemote(existingRepository, originGitUrl, hkgGitUrl, centralBranchForPush);
+        System.out.println(existingRepository.getBranch());
     }
 
-    private static void pushChangesToUpstream(Repository existingRepository, String hkgGitUrl, String hkgRemoteRepository) throws GitAPIException, URISyntaxException {
+    public static void pushChangesToRemote(Repository existingRepository, String gitUrlForOrigin, String gitUrlForCentral, String centralBranchForPush) throws GitAPIException, URISyntaxException, IOException {
+        pushChangesToOrigin(existingRepository, gitUrlForOrigin);
+        pushChangesToUpstream(existingRepository, gitUrlForCentral, centralBranchForPush);
+    }
+
+    private static void pushChangesToUpstream(Repository existingRepository, String gitUrlForCentral, String centralBranchForPush) throws GitAPIException, URISyntaxException, IOException {
         System.out.println("Starting push changes to HKG git server");
         try (Git git = new Git(existingRepository)) {
             // add remote repo:
             RemoteAddCommand remoteAddCommand = git.remoteAdd();
-            remoteAddCommand.setUri(new URIish(hkgGitUrl));
+            remoteAddCommand.setUri(new URIish(gitUrlForCentral));
             // you can add more settings here if needed
             remoteAddCommand.call();
 
             // push to remote:
             PushCommand pushCommand = git.push();
             //need to set remote, or it will push to default remote "origin"
-            pushCommand.setRemote(hkgRemoteRepository);
+            pushCommand.setRemote(centralBranchForPush.split("/")[0]);
+            //RefSpec: is the mapping relationship between local branch and central branch
+            pushCommand.setRefSpecs(new RefSpec(existingRepository.getBranch() + ":" + centralBranchForPush.split("/")[1]));
             //pushCommand.setCredentialsProvider(new UsernamePasswordCredentialsProvider("username", "password"));
             // you can add more settings here if needed
             pushCommand.call();
@@ -41,14 +53,14 @@ public class PushCommitToRemote {
         System.out.println("End push changes to HKG git server...");
     }
 
-    private static void pushChangesToOrigin(Repository existingRepository, String originGitUrl) throws GitAPIException, URISyntaxException, IOException {
+    private static void pushChangesToOrigin(Repository existingRepository, String gitUrlForOrigin) throws GitAPIException, URISyntaxException, IOException {
         System.out.println("Starting push changes to origin...");
         try (Git git = new Git(existingRepository)) {
             // add remote repo:
             RemoteAddCommand remoteAddCommand = git.remoteAdd();
-            remoteAddCommand.setUri(new URIish(originGitUrl));
+            remoteAddCommand.setUri(new URIish(gitUrlForOrigin));
             remoteAddCommand.call();
-            // push to remote:default is origin
+            // push to remote:default is origin, can find in the source code (pushCommand.call())
             PushCommand pushCommand = git.push();
             //pushCommand.setCredentialsProvider(new UsernamePasswordCredentialsProvider("username", "password"));
             // you can add more settings here if needed
